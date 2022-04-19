a21a1f194f78eac8b73191066a4c0e47ce6ecb4e
Victor <santtosvictor42@gmail.com>
2022-04-19 15:42:55 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+const date = new Date()
+const hours = 15
+async function generateRandomImage() {
+    
+    const morning = hours > 6 && hours < 12
+    const daytime = hours > 12 && hours < 18
+    const nigth = hours > 18 && hours < 5
+    if(daytime) {
+        fs.readFile('./uploads/bom-dia/34.jpg', 'utf-8', (err, data) => {
+            
+            const content = ``
+            
+            fs.writeFile('README.md', data, (err) => {
+                if(err) {
+                    console.log(err);
+                }
+            });
+             
+        })
+    }
+   
+    
+    // fs.readdir('./uploads', (err, files) => {
+    //     if (err)
+    //       console.log(err);
+    //     else {
+    //       console.log("\nCurrent directory filenames:");
+    //       files.forEach(file => {
+    //         console.log(file);
+    //       })
+    //     }
+    //   })
+}
+
+generateRandomImage()
+
