012779794ebe55411e2fc4247fc1ff8fee2f8d4f
Victor Santos <santtosvictor42@gmail.com>
2024-04-25 21:17:27 -0300

    chore: ajustado readme gerado

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-<img src="./uploads/nigth/7.jpg" width="600" height="300">
+<div style="display: block;width:600px;margin: 0 auto" ><img src="./uploads/nigth/3.jpg" width="600" height="300">
+    <p style="font-size:14px;color:#white">*Leve em consideração a imagem, isso e apenas para complimenta-lo pela visita ao meu perfil é propagar um cadinho de humor.</p>
+    </div>
--- index.js
+++ index.js
@@ -1,55 +1,48 @@
-const fs = require('fs')
-const {resolve} = require('path')
-const date = new Date()
-const hours = date.getHours()
+const fs = require("fs");
+const { resolve } = require("path");
+const date = new Date();
+const hours = date.getHours();
 const currentTime = (timeParam) => {
-    let time = ''
-    
-    const morning = hours >= 5 && hours <= 11
-    const daytime = hours >= 11 && hours <= 17
-    const nigth = hours >= 17 && hours <= 23
-    const dawn = hours >= 0 && hours < 4
+  let time = "";
 
-    
-    if(morning) time = 'morning';
-    if(daytime) time = 'daytime';
-    if(nigth || dawn) time = 'nigth';
-    
-    return {
-        current: time === timeParam ? true : false,
-        path: time
-    }
-}
+  const morning = hours >= 5 && hours <= 11;
+  const daytime = hours >= 11 && hours <= 17;
+  const nigth = hours >= 17 && hours <= 23;
+  const dawn = hours >= 0 && hours < 4;
+
+  if (morning) time = "morning";
+  if (daytime) time = "daytime";
+  if (nigth || dawn) time = "nigth";
+
+  return {
+    current: time === timeParam ? true : false,
+    path: time,
+  };
+};
 const generateImage = (path) => {
 
-    return fs.writeFile('README.md', path ,(err) => {
-            if(err) {
-                console.log(err);
-            }
-    });
-  
-    
-    
-}
+    const message = `<div style="display: block;width:600px;margin: 0 auto" >${path}
+    <p style="font-size:14px;color:#white">*Leve em consideração a imagem, isso e apenas para complimenta-lo pela visita ao meu perfil é propagar um cadinho de humor.</p>
+    </div>`
 
+  return fs.writeFile("README.md", message, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+};
 
-const generateRandomNumber = (max, min) => Math.random() * (max - min) + min
-
+const generateRandomNumber = (max, min) => Math.random() * (max - min) + min;
 
 const generateRandomImage = () => {
-    const value = generateRandomNumber(10, 1)
-    const pathDir = currentTime().path
-    const current = (time) => currentTime(time).current
-    console.log(pathDir)
+  const value = generateRandomNumber(10, 1);
+  const pathDir = currentTime().path;
+  const current = (time) => currentTime(time).current;
+  const path = `<img src="./uploads/${pathDir}/${value.toFixed()}.jpg" width="600" height="300">`;
 
-    const path = `<img src="./uploads/${pathDir}/${value.toFixed()}.jpg" width="600" height="300">`
-   
-
-    if(current('morning')) return generateImage(path)
-    if(current('daytime')) return generateImage(path)
-    if(current('nigth')) return generateImage(path)
-    
-
-}
+  if (current("morning")) return generateImage(path);
+  if (current("daytime")) return generateImage(path);
+  if (current("nigth")) return generateImage(path);
+};
 
 generateRandomImage();
